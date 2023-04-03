6b99352ff0bc7b4ada83b34902c7f5dee9013b96
Liwu-di <liwudi@liwudi.fun>
2023-04-03 17:58:48 +0800

    vc

--- virtual-dast.py
+++ virtual-dast.py
@@ -664,7 +664,8 @@ def model_train(args, model, optimizer, train_dataloader, val_dataloader, test_d
                                                                                                     optimizer,
                                                                                                     total_step,
                                                                                                     start_step,
-                                                                                                    args.need_road)
+                                                                                                    args.need_road,
+                                                                                                    train_dataloader, val_dataloader, test_dataloader, type)
         log(f'Epoch {epoch} | acc_train: {train_acc: .4f} | mae_train: {mae_train: .4f} | rmse_train: {rmse_train: .4f} | mae_val: {mae_val: .4f} | rmse_val: {rmse_val: .4f} | mae_test: {mae_test: .4f} | rmse_test: {rmse_test: .4f} | mape_test: {mape_test: .4f} | Time(s) {dur[-1]: .4f}')
         epoch += 1
         acc.append(train_acc)
