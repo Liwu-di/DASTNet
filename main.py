9f201e9c8066d1129b2fce5e8eebae09a571ea91
Liwu-di <liwudi@liwudi.fun>
2023-04-04 23:53:42 +0800

    dast-selective

--- main.py
+++ main.py
@@ -441,11 +441,11 @@ if args.c != "default":
     if args.need_remark == 1:
         record.update(record_id, get_timestamp(),
                       "%.4f,%.4f,%.4f" %
-                      (test_rmse, test_mae, test_mape / 100),
+                      (test_rmse, test_mae, test_mape * 100),
                       remark="{}C {} {} {} {}".format("2" if args.need_third == 0 else "3", str(args.data_amount),
                                                       args.dataname, args.datatype, args.machine_code))
     else:
         record.update(record_id, get_timestamp(),
                       "%.4f,%.4f, %.4f" %
-                      (test_rmse, test_mae, test_mape / 100),
+                      (test_rmse, test_mae, test_mape * 100),
                       remark="{}".format(args.machine_code))
