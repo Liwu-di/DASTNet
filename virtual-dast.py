e5b0b3ef3753b5785ba1d8065f4daed8894a1172
Liwu-di <liwudi@liwudi.fun>
2023-04-04 12:13:13 +0800

    vc

--- virtual-dast.py
+++ virtual-dast.py
@@ -1099,7 +1099,6 @@ def model_train(args, model, optimizer, train_dataloader, val_dataloader, test_d
                 source_weights_ma = torch.ones_like(source_weights, device=device, requires_grad=False)
             source_weights_ma = cross_ma_param * source_weights_ma + (1 - cross_ma_param) * source_weights
             log(source_weights_ma.mean())
-            source_weights_ma = torch.ones_like(source_weights_ma, device=device, requires_grad=False)
         else:
             source_weights_ma = None
         start_step = epoch * step_per_epoch
