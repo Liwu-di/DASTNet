f55ee5431d4d8337760570ce09a0969ecde8fba4
Liwu-di <liwudi@liwudi.fun>
2023-04-09 23:00:59 +0800

    dast-selective

--- virtual-dast.py
+++ virtual-dast.py
@@ -1027,7 +1027,7 @@ def train(dur, model, optimizer, total_step, start_step, need_road, train_datalo
                                                         maskp=mask, weight=weight)
 
         if type == 'pretrain':
-            loss = mae_train + args.beta * (args.theta * domain_loss)
+            loss = mae_train + args.beta * (args.theta * domain_loss) * torch.from_numpy(mask).to(feat.device).reshape((-1)) * weight
         elif type == 'fine-tune':
             loss = mae_train
 
