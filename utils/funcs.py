2d4bc68be3bc45bfc4bdd44f61ec85ff73e207a5
Liwu-di <liwudi@liwudi.fun>
2023-04-25 15:00:16 +0800

    commands

--- utils/funcs.py
+++ utils/funcs.py
@@ -471,7 +471,7 @@ def masked_loss(y_pred, y_true, maskp=None, weight=None):
     mse_loss = torch.square(y_pred - y_true)
     #y_true = torch.where(y_true.abs() < torch.tensor(0.01, dtype=y_true.dtype, device=y_true.device), torch.tensor(0, dtype=y_true.dtype, device=y_true.device), y_true)
     a = []
-    for i in range(1, 100, 1):
+    for i in range(1, 20, 1):
         y_true = y_true + (torch.ones(y_true.shape) * float(i / 100)).to(y_pred.device)
         mae_pe = mae_loss[:, torch.from_numpy(maskp).to(y_pred.device).reshape((-1))]
         ytrue_pe = y_true[:, torch.from_numpy(maskp).to(y_pred.device).reshape((-1))]
