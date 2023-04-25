794ce00abaca19c8dc2f0bff99599effa07e6490
Liwu-di <liwudi@liwudi.fun>
2023-04-25 17:14:14 +0800

    commands

--- utils/funcs.py
+++ utils/funcs.py
@@ -481,9 +481,9 @@ def masked_loss(y_pred, y_true, maskp=None, weight=None):
     #     print(a)
     #     flag = False
     # y_true = y_true + (torch.ones(y_true.shape) * float(5)).to(y_pred.device)
-    # mae_pe = mae_loss[:, torch.from_numpy(maskp).to(y_pred.device).reshape((-1))]
-    # ytrue_pe = y_true[:, torch.from_numpy(maskp).to(y_pred.device).reshape((-1))]
-    # mape_loss = mae_pe / ytrue_pe.abs()
+    mae_pe = mae_loss[:, torch.from_numpy(maskp).to(y_pred.device).reshape((-1))]
+    ytrue_pe = y_true[:, torch.from_numpy(maskp).to(y_pred.device).reshape((-1))]
+    mape_loss = mae_pe / ytrue_pe.abs()
     mape_loss = torch.where(mape_loss.abs() > torch.tensor(1, dtype=mape_loss.dtype, device=y_true.device),
                          torch.tensor(0, dtype=mape_loss.dtype, device=mape_loss.device), mape_loss)
     mape_loss = mae_loss / y_true.abs()
